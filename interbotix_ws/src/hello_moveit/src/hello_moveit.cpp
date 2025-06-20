c846b14512a1ed247194b5f12f325a9c8081e904
Logan <favoringbasil55@gmail.com>
2025-06-20 10:52:13 -0400

    pre-work commit

--- interbotix_ws/src/hello_moveit/src/hello_moveit.cpp
+++ interbotix_ws/src/hello_moveit/src/hello_moveit.cpp
@@ -17,7 +17,7 @@ int main(int argc, char * argv[])
 
   // Create the MoveIt MoveGroup Interface
   using moveit::planning_interface::MoveGroupInterface;
-  auto move_group_interface = MoveGroupInterface(node, "panda_arm");
+  auto move_group_interface = MoveGroupInterface(node, "interbotix_arm");
 
   // Set a target Pose
   auto const target_pose = []{
